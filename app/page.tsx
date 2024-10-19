68dcaf6ba63e55dba437e5c02b56357d51a42d54
Mirlan Boroshilov <boroshilov.mirlan@gmail.com>
2024-10-19 05:30:58 -0700

    New

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,12 @@
-import Image from 'next/image';
-import Link from 'next/link';
-
+import Image from "next/image";
+import Link from "next/link";
+import { BackgroundLines } from "@/components/ui/background-lines";
 export default function Home() {
-    return <div>Home!!!</div>;
+  return (
+    <div>
+      <BackgroundLines className="h-screen">
+        <h1>Home!!!</h1>
+      </BackgroundLines>
+    </div>
+  );
 }
